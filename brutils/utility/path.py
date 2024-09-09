9e38595a1bdf3536bd9b13f0e41be0121e14c1f0
Behnam Rasoolian <behnam.rasoulian@gmail.com>
2024-09-09 15:56:28 -0400

    added path.py as library to support path manipulation

---
+++ brutils/utility/path.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path, PosixPath
+
+
+class PathManager(PosixPath):
+    def __init__(self, path):
+        self.path = Path(path)
+
+    @property
+    def extension(self):
+        return str(self).split(".")[-1]
+
+    @property
+    def dirname(self):
+        return os.path.dirname(self)
+
+    @property
+    def basename(self):
+        return os.path.basename(self)
+
+    def change_extension(self, new_extension):
+        out = str(self).split(".")[:-1] + [new_extension]
+        return self.__class__(".".join(out))
+
+    def change_dir(self, new_dir):
+        return Path(new_dir) / Path(self.basename)
