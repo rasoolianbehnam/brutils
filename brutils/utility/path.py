7c158e3d1947182a8a296cfd19f8b5e79f7183fe
Behnam Rasoolian <behnam.rasoulian@gmail.com>
2024-09-23 10:00:03 -0400

    added path utilities

--- brutils/utility/path.py
+++ brutils/utility/path.py
@@ -18,9 +18,16 @@ class PathManager(PosixPath):
     def basename(self):
         return os.path.basename(self)
 
+    @property
+    def remove_extension(self):
+        return self.change_extension("")
+
     def change_extension(self, new_extension):
         out = str(self).split(".")[:-1] + [new_extension]
         return self.__class__(".".join(out))
+
+    def append_to_name(self, prefix="", suffix=""):
+        pass
 
     def change_dir(self, new_dir):
         return self.__class__(Path(new_dir) / Path(self.basename))
