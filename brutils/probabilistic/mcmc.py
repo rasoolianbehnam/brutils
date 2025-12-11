02493184c4b4f62bccd141ec2e06434474db64ca
Behnam Rasoolian <behnam.rasoulian@gmail.com>
2025-12-11 16:55:48 -0500

    added functions

--- brutils/probabilistic/mcmc.py
+++ brutils/probabilistic/mcmc.py
@@ -1,10 +1,30 @@
 import time
 import jax
 import blackjax
+import arviz as az
 from tensorflow_probability.substrates import jax as tfp
 
 tfb = tfp.bijectors
 jnp = jax.numpy
+
+
+def check_rhat(trace, *, thresh=None, ignore_nan=False, bijectors=None):
+    trace = dict(trace)
+    if bijectors is not None:
+        for k in bijectors:
+            trace[k] = bijectors[k].inverse(trace[k])
+
+    def fn(x):
+        y = jnp.abs(tfp.mcmc.potential_scale_reduction(x) - 1)
+        out = jnp.array(y).squeeze()
+        if ignore_nan and jnp.isnan(out).sum() > 0:
+            out = jnp.where(jnp.isnan(out), 0, out)
+        return out.max()
+
+    out = jax.tree.map(fn, trace)
+    if thresh is not None:
+        out = jax.tree.map(lambda x: x < thresh, out)
+    return out
 
 
 def inference_loop(rng_key, kernel, initial_state, num_samples):
@@ -28,21 +48,24 @@ inference_loop_multiple_chains = jax.pmap(
 def sample_potential_fn(
     potential_fn,
     initial_params,
-    bijectors,
+    bijectors=None,
     rng_key=None,
     num_warmup_steps=200,
     num_steps=1000,
-    n_chains=4,
+    num_chains=4,
     target_acceptance_rate=0.8,
 ):
     if rng_key is None:
         rng_key = generate_key()
 
-    if isinstance(bijectors, list):
+    if isinstance(bijectors, (list, tuple)):
         tmp = {}
         for k, bij in zip(initial_params.keys(), bijectors):
             tmp[k] = bij
         bijectors = tmp
+
+    for k in initial_params:
+        initial_params[k] = bijectors[k].inverse(initial_params[k])
 
     @jax.jit
     def unconstrained_potential_fn(args):
@@ -60,7 +83,7 @@ def sample_potential_fn(
         unconstrained_potential_fn,
         initial_params,
         num_warmup_steps,
-        n_chains,
+        num_chains,
         target_acceptance_rate,
         warmup_key,
     )
@@ -68,7 +91,7 @@ def sample_potential_fn(
     rng_key, sample_key = jax.random.split(rng_key)
 
     rng_key, sample_key = jax.random.split(rng_key)
-    sample_keys = jax.random.split(sample_key, n_chains)
+    sample_keys = jax.random.split(sample_key, num_chains)
 
     chain_states = inference_loop_multiple_chains(
         sample_keys, nuts.step, initial_states, num_steps
@@ -91,7 +114,7 @@ def adapt_nuts(
     unconstrained_potential_fn,
     initial_params,
     num_warmup_steps=200,
-    n_chains=4,
+    num_chains=4,
     target_acceptance_rate=0.8,
     rng_key=None,
 ):
@@ -108,9 +131,36 @@ def adapt_nuts(
     nuts = blackjax.nuts(unconstrained_potential_fn, **parameters)
     initial_states = jax.vmap(nuts.init, in_axes=(0))(
         {
-            k: jnp.einsum("...c->c...", v[..., None] * jnp.ones(n_chains))
+            k: jnp.einsum("...c->c...", v[..., None] * jnp.ones(num_chains))
             for k, v in last_state.position.items()
         }
     )
 
     return initial_states, parameters
+
+
+def sample_tfp(target, initial_states=None, bijectors=None, **sampling_kwargs):
+    if bijectors is None:
+        bijectors = target.experimental_default_event_space_bijector().bijectors
+    if initial_states is None:
+        initial_states = target.sample(seed=generate_key())._asdict()
+
+    log_prob_fn = lambda kwargs: target.log_prob(**kwargs)
+    out = sample_potential_fn(log_prob_fn, initial_states, bijectors, **sampling_kwargs)
+    r_hat = check_rhat(out.position)
+    return az.from_dict(
+        out.position, log_likelihood=out.logdensity_grad, sample_stats=r_hat
+    )
+
+
+def to_dict(az_dataset):
+    return {
+        k: jnp.array(v["data"]) for k, v in az_dataset.to_dict()["data_vars"].items()
+    }
+
+
+def asdict(d):
+    return to_dict(d.posterior)
+
+
+az.data.inference_data.InferenceData.dict = property(asdict)
